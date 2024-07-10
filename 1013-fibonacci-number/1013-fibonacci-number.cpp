7699001cfd91fd25c2394602d148afea51ba50eb
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-10 10:08:42 +0530

    Time: 3 ms (67.43%), Space: 7.1 MB (34.26%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int cal(int n){
-        if(n==0){
-            return 0;
-        }
-        if(n==1){
-            return 1;
-        }
-        // if(dp[n]!=-1){
-        //     return dp[n];
-        // }
-        return  cal(n-1)+cal(n-2);
-        // return dp[n];
-    }
     int fib(int n) {
-        if(n==0){
-            return 0;
+       if(n==0) {
+        return 0;
+       }
+       else if(n==1) return 1;
+       else{
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]= dp[i-1]+dp[i-2];
         }
-        else if(n==1){
-            return 1;
-        }
-        else{
-            // vector<int> dp(n+1,-1);
-            // dp[0]=0;
-            // dp[1]=1;
-            int calculate = cal(n);
-            return calculate;   
-        }
+        return dp[n];
+       }
+        
     }
 };
