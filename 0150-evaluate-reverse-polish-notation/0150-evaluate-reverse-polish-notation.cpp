b23e37474e2e560757931b2fccb26b47d2be6afb
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-21 23:20:31 +0530

    Time: 10 ms (28.48%), Space: 16.1 MB (11.82%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -9,25 +9,24 @@ public:
         for(int i=0;i<tokens.size();i++){
             if(tokens[i]=="+"||tokens[i]=="-"||tokens[i]=="*"||tokens[i]=="/"){
                 vector<int> num(2);
-                int num0=0,num1=0;
-                num0 = hello.top();
-                hello.pop();
-                num1 = hello.top();
-                hello.pop();
+                for(int i=0;i<2;i++){
+                    num[i] = hello.top();
+                    hello.pop();
+                }
                 if(tokens[i]=="+"){
-                    res = num0+num1;
+                    res = num[0]+num[1];
                     hello.push(res);
                 }
                 else if(tokens[i]=="-"){
-                    res = num1-num0;
+                    res = num[1]-num[0];
                     hello.push(res);
                 }
                 else if(tokens[i]=="*"){
-                    res = num1*num0;
+                    res = num[1]*num[0];
                     hello.push(res);
                 }
                 else if(tokens[i]=="/"){
-                    res = num1/num0;
+                    res = num[1]/num[0];
                     hello.push(res);
                 }
                 
