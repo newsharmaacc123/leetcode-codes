d05b93d72f935cf186497bc4ca9ab4e5da7c0db4
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-24 15:41:42 +0530

    Time: 0 ms (100%), Space: 9.5 MB (12.44%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
-    int get_max(vector<int>&dp){
-        int maxi = 0;
-        for(int i=0;i<dp.size();i++){
-            maxi = max(maxi,dp[i]);
-        }
-        return maxi;
+    int cal(vector<int>& nums,int count,vector<int>& dp){
+if(count==0){
+        return nums[0];
     }
+    if(count<0){
+        return 0;
+    }
+    if(dp[count]!=-1){
+        return dp[count];
+    }
+    int incl = cal(nums,count-2,dp)+nums[count];
+    int excl = cal(nums,count-1,dp)+0;
+    return dp[count] = max(incl,excl);
+}
+    
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        if(n==1){
-            return nums[0];
-        }
-        else if(n==2){
-            if(nums[0]>nums[1]){
-                return nums[0];
-            }
-            else{
-                return nums[1];
-            }
-        }
-        else{
-            dp[0] = nums[0];
-            dp[1] = nums[1];
-            for(int i=2;i<n;i++){
-                int j = 0,maxi=0;
-                while(j<i-1){
-                    maxi = max(dp[j],maxi) ;
-                    j++;
-                }
-                dp[i] = maxi+nums[i];
-            }
-        }
-        int max_num = get_max(dp);
-        return max_num;
+        vector<int> dp(nums.size()+1,-1);
+        return cal(nums,nums.size()-1,dp);
     }
 };
