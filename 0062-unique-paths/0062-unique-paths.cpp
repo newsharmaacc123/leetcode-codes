2de37113aedabefb4adfe45471eff9d55d430d7a
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-25 16:44:55 +0530

    Time: 3 ms (33.97%), Space: 7.7 MB (52.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,24 +1,7 @@
 #define num 2000000000;
 class Solution {
 public:
-    int cal(int m,int n,vector<vector<int>>dp){
-        if(m==0&&n==0){
-            return 1;
-        }
-        if(m<0||n<0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        int left = cal(m-1,n,dp);
-        int right = cal(m,n-1,dp);
-        dp[m][n]=left+right;
-        return dp[m][n];
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-
         vector<int> prev(n,0);
 
         for(int i =0;i<m;i++){
