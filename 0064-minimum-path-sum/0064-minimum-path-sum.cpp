71fe267dda4f23f4b2a19626b4eb963941276adb
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-26 19:14:29 +0530

    Time: 12 ms (7.37%), Space: 13 MB (6.39%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -11,7 +11,15 @@ public:
     }
     int minPathSum(vector<vector<int>>& grid) {
         int row = grid.size(),col = grid[0].size();
-        vector<vector<int>> dp(row,vector<int>(col,-1));
-        return cal(row-1,col-1,grid,dp);
+        vector<vector<int>> dp(row,vector<int>(col,INT_MAX));
+        // return cal(row-1,col-1,grid,dp);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(i==0&&j==0) dp[i][j] = grid[0][0];
+                if(i>0) dp[i][j] = min(dp[i][j],dp[i-1][j]+grid[i][j]);
+                if(j>0) dp[i][j] = min(dp[i][j],dp[i][j-1]+grid[i][j]);
+            }
+        }
+        return dp[row-1][col-1];
     }
 };
