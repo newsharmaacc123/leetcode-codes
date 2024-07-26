ef7cff4e2834179d4d67d72b44db8a37d3c89e13
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-26 13:56:30 +0530

    Time: 2 ms (44.46%), Space: 10.7 MB (11.62%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry=1;
+        for(int i=digits.size()-1;i>=0;i--){
+            if(digits[i]!=9){
+                digits[i] = digits[i]+carry;
+                carry = 0;
+            }
+            else{
+                if(carry==1){
+                    digits[i] = 0;
+                    carry =1;
+                }
+            }
+        }
+        if(carry ==1){
+            digits[0]++;
+            digits.push_back(0);
+            return digits;
+        }
+        else{
+            return digits;
+        }
+    }
+};
