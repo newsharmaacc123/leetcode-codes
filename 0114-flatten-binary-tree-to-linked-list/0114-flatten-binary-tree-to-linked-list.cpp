bf121a76ca30d8d4c4b9bed2435a03071fe44728
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-27 11:38:35 +0530

    Update 0114-flatten-binary-tree-to-linked-list.cpp

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -12,16 +12,20 @@
 class Solution {
 public:
     void flatten(TreeNode* root) {
-        if(!root) return ;
-        stack<TreeNode*> st;
-        st.push(root);
-        while(!st.empty()){
-            auto curr = st.top();
-            st.pop();
-            if(curr->right!=NULL) st.push(curr->right) ;
-            if(curr->left!=NULL) st.push(curr->left) ;
-            if(!st.empty()) curr->right = st.top();
-            curr->left=NULL;
+        if(root == NULL) return;
+        
+        TreeNode* curr = root;
+        while(curr) {
+            if(curr->left != NULL) {
+                TreeNode* prev = curr->left;
+                while(prev->right) {
+                    prev = prev->right;
+                }
+                prev->right = curr->right;
+                curr->right = curr->left;
+                curr->left = NULL;
+            }
+            curr = curr->right;
         }
     }
 };
