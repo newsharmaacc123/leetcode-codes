2430f59a195f9553c1b0db7daf619de18ea8630b
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-27 17:12:51 +0530

    Time: 4 ms (73.4%), Space: 11.8 MB (12.35%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int  sum=0;
-    int cal(int row,int col,vector<vector<int>>& triangle,vector<vector<int>>&dp){
-        if(row==triangle.size()-1) return triangle[row][col];
-        if(col==triangle[row].size()) return INT_MAX;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int down = cal(row+1,col,triangle,dp);
-        int diagonal = cal(row+1,col+1,triangle,dp);
-        dp[row][col] =  triangle[row][col]+min(down,diagonal);
-        return dp[row][col];
+    // int  sum=0;
+    // int cal(int row,int col,vector<vector<int>>& triangle,vector<vector<int>>&dp){
+    //     if(row==triangle.size()-1) return triangle[row][col];
+    //     if(col==triangle[row].size()) return INT_MAX;
+    //     if(dp[row][col]!=-1) return dp[row][col];
+    //     int down = cal(row+1,col,triangle,dp);
+    //     int diagonal = cal(row+1,col+1,triangle,dp);
+    //     dp[row][col] =  triangle[row][col]+min(down,diagonal);
+    //     return dp[row][col];
 
-    }
+    // }
     int minimumTotal(vector<vector<int>>& triangle) {
         int row = triangle.size();
         int col = triangle[row-1].size();
