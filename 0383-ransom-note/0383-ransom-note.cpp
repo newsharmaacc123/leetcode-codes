02fe35106c230aa89bba5c2dc972749232e07183
newsharmaacc123 <161288957+newsharmaacc123@users.noreply.github.com>
2024-07-30 00:40:56 +0530

    Time: 30 ms (6.14%), Space: 11.7 MB (5.22%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int> magzine_map;
+        for(char c:magazine){
+            if(magzine_map.find(c)==magzine_map.end()){
+                magzine_map[c] =1;
+            }
+            else{
+                magzine_map[c]++;
+            }
+        }
+        for(char c:ransomNote){
+            if(magzine_map.find(c)!=magzine_map.end()&&magzine_map[c]>0){
+                magzine_map[c]--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+};
